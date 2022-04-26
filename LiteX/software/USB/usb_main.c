d94d5ec1ba715fcad4ffef5e311685c675e4e2e4
Victor Suarez Rovere <suarezvictor@gmail.com>
2022-04-26 20:51:47 -0300

    tried optimizations (forced -O3, move .text to sram) - still 2MHz

--- LiteX/software/USB/usb_main.c
+++ LiteX/software/USB/usb_main.c
@@ -19,6 +19,14 @@ void _putchar(char c) { uart_write(c); }
 #include "usb_host.h"
 
 int main(int argc, char **argv) {
+    uart_init();
+    irq_setmask(0);
+    irq_setie(1);
+
+    if((size_t)&main < 0x40000000)
+      printf("sram\n");
+    else
+      printf("dram\n");
     hal_gpio_set_direction(8, GPIO_MODE_OUTPUT);
     hal_gpio_set_direction(12, GPIO_MODE_OUTPUT);
     for(;;)
@@ -27,10 +35,6 @@ int main(int argc, char **argv) {
       hal_gpio_set_level(8, 1); hal_gpio_set_level(12, 0);
     }
 
-    irq_setmask(0);
-    irq_setie(1);
-
-    uart_init();
 /*
     printf("myfloat %d %d %d %f %d %d %g %d\n", 1, 4, 5, 1.1f, -10, -11, 2.3, -12);
     printf("myfloat %d %d %d %f %d %g %d %d\n", 1, 4, 5, 1.1f, -10, 2.3, -11, -12);
